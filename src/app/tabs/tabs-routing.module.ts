31f8bc24ab0ef68c97e385b7ec8c8c1069de420f
Captain Storm <CaptainStorm21@users.noreply.github.com>
2019-12-21 14:56:48 -0800

    Add files via upload

---
+++ src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'todo',
+        children: [
+          {
+            path: '',
+            loadChildren: '../todo/todo.module#TodoPageModule'
+          }
+        ]
+      },
+      {
+        path: 'about',
+        children: [
+          {
+            path: '',
+            loadChildren: '../about/about.module#AboutPageModule'
+          }
+        ]
+      },
+      {
+        path: 'contact',
+        children: [
+          {
+            path: '',
+            loadChildren: '../contact/contact.module#ContactPageModule'
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/todo',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/todo',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule {}
